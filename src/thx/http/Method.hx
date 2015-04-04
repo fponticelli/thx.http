023bbcb6dc1992640fb22bd6a2ac6b7063275d28
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-03 23:31:25 -0600

    updated methods list

--- src/thx/http/Method.hx
+++ src/thx/http/Method.hx
@@ -1,9 +1,13 @@
 package thx.http;
 
 @:enum abstract Method(String) to String {
+	var Connect = "CONNECT";
+	var Delete = "DELETE";
 	var Get = "GET";
+	var Head = "HEAD";
+	var Options = "OPTIONS";
+	var Patch = "PATCH";
 	var Post = "POST";
 	var Put = "PUT";
-	var Delete = "DELETE";
-	// TODO add remaining
+	var Trace = "TRACE";
 }
