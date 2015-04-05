e63667964e8e15c242b1a6c8771b372658eaeb63
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-05 11:53:47 -0600

    added key, value to Header

--- src/thx/http/Header.hx
+++ src/thx/http/Header.hx
@@ -12,7 +12,7 @@ abstract Header(Tuple2<String, String>) to Tuple2<String, String> {
 
 	public static function normalize(t : Tuple2<String, String>) {
 		t._0 = normalizeKey(t._0);
-		t._1 = normalizeKey(t._1);
+		t._1 = normalizeValue(t._1);
 		return t;
 	}
 
@@ -22,6 +22,21 @@ abstract Header(Tuple2<String, String>) to Tuple2<String, String> {
 	public static function normalizeValue(value : String, ?key : String = " ")
 		return CRLF_PATTERN.replace(value, Const.CRLF);
 
+	public var key(get, set) : String;
+	public var value(get, set) : String;
+
 	inline function new(t : Tuple2<String, String>)
 		this = t;
+
+	inline function get_key()
+		return this._0;
+
+	inline function set_key(v : String)
+		return this._0 = normalizeKey(v);
+
+	inline function get_value()
+		return this._1;
+
+	inline function set_value(v : String)
+		return this._1 = normalizeValue(v);
 }
