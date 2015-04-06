410ab2c1641ddc2ce0cbaf92ce19cec5fd705948
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-05 20:30:01 -0600

    fixed HaxeRequest

--- src/thx/http/core/HaxeRequest.hx
+++ src/thx/http/core/HaxeRequest.hx
@@ -1,14 +1,18 @@
 package thx.http.core;
 
+using thx.core.Arrays;
 import thx.core.Functions;
 
 class HaxeRequest {
 	public static function make(requestInfo : RequestInfo, requestHandler : RequestHandler) : Void -> Void {
 		var req = new haxe.Http(requestInfo.url);
-		for(key in requestInfo.headers.keys())
-			req.setHeader(key, requestInfo.headers.get(key));
-		req.onStatus = requestHandler.statusHandler;
-		//req.onData = requestHandler.dataHandler;
+		(requestInfo.headers : Array<Header>)
+			.pluck(req.addHeader(_.key, _.value));
+
+		var buf = "";
+		req.onData = function(data) buf += data;
+		req.onStatus = function(status) requestHandler.statusHandler(status, buf);
+
 		//req.onError = requestHandler.errorHandler;
 		switch requestInfo.method {
 			case Get: req.request(false);
