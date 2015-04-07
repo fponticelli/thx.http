a44c8ac635336751756ceaf5cadf2b86db200b09
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-06 22:12:05 -0600

    working on NodeJS implementation

--- src/thx/http/core/NodeJSRequest.hx
+++ src/thx/http/core/NodeJSRequest.hx
@@ -2,22 +2,34 @@ package thx.http.core;
 
 import js.node.Http;
 import js.node.Https;
+import thx.core.Error;
 
 class NodeJSRequest {
-	public static function make(requestInfo : RequestInfo, requestHandler : RequestHandler) : Void -> Void {
-		function callback(res) {
-			var buf = "";
+	public static function make(requestInfo : RequestInfo, callback : Response -> Void, error : Error -> Void) : Void -> Void {
+		function callbackResponse(res : js.node.http.IncomingMessage) {
+			//var buf = "";
+			var first = true;
 			res.on("data", function(chunk) {
+				if(first) {
+					first = false;
+					callback(new NodeJSResponse(res.statusCode));
+				}
+				trace(res.statusCode);
 				trace(Std.is(chunk, String));
-				buf += chunk;
+				trace(chunk);
+				//buf += chunk;
 			});
+
 			res.on("end", function(_) {
-				requestHandler.statusHandler(res.statusCode, buf);
+				if(first) {
+					first = false;
+					callback(new NodeJSResponse(res.statusCode));
+				}
 			});
 		}
 
 		var url = requestInfo.url,
-				req = switch url.protocol {
+				req : js.node.http.ClientRequest = switch url.protocol {
 					case "http":
 						Http.request({
 							hostname: url.hostName,
@@ -31,7 +43,7 @@ class NodeJSRequest {
 							//agent: Controls Agent behavior. When an Agent is used request will default to Connection: keep-alive. Possible values:
 							//keepAlive: {Boolean} Keep sockets around in a pool to be used by other requests in the future. Default = false
 							//keepAliveMsecs: {Integer} When using HTTP KeepAlive, how often to send TCP KeepAlive packets over sockets being kept alive. Default = 1000. Only relevant if keepAlive is set to true.
-						}, callback);
+						}, callbackResponse);
 					case "https":
 						Https.request({
 							hostname: url.hostName,
@@ -45,11 +57,35 @@ class NodeJSRequest {
 							//agent: Controls Agent behavior. When an Agent is used request will default to Connection: keep-alive. Possible values:
 							//keepAlive: {Boolean} Keep sockets around in a pool to be used by other requests in the future. Default = false
 							//keepAliveMsecs: {Integer} When using HTTP KeepAlive, how often to send TCP KeepAlive packets over sockets being kept alive. Default = 1000. Only relevant if keepAlive is set to true.
-						}, callback);
+						}, callbackResponse);
 					case other:
 						throw 'unexpected protocol $other';
-				}
+				};
+		req.on("error", function(e) error(Error.fromDynamic(e)));
 		req.end();
 		return req.abort;
+	}
+}
+
+class NodeJSResponse implements thx.http.Response {
+	@:isVar public var body(get, null) : ResponseBody;
+	@:isVar public var statusCode(get, null) : Int;
+	@:isVar public var statusText(get, null) : String;
+	@:isVar public var headers(get, null) : Headers;
+	var getRawHeaders : Void -> String;
+	public function new(statusCode : Int/*, body : String, getRawHeaders : Void -> String*/) {
+		this.statusCode = statusCode;
+		//this.body = null == body || "" == body ? NoBody : BodyString(body);
+		//this.getRawHeaders = getRawHeaders;
+	}
+
+	function get_body() return ResponseBody.NoBody;
+	function get_statusCode() return statusCode;
+	function get_statusText() return null;
+	function get_headers() {
+		if(null == headers) {
+			//headers = getRawHeaders();
+		}
+		return Headers.empty();
 	}
 }
