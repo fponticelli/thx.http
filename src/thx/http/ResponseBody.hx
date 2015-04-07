6234df1a2a78843ea3a1bb77b021ba7e3b6b10bb
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-06 20:57:53 -0600

    changed ResponseBody io direction

--- src/thx/http/ResponseBody.hx
+++ src/thx/http/ResponseBody.hx
@@ -1,11 +1,11 @@
 package thx.http;
 
 import haxe.io.Bytes;
-import haxe.io.Output;
+import haxe.io.Input;
 
 enum ResponseBody {
 	NoBody;
 	BodyString(s : String, ?encoding : String);
 	BodyBytes(b : Bytes);
-	BodyStream(s : Output);
+	BodyStream(s : Input);
 }
