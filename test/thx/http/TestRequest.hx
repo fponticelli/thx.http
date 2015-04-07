964064dc3dbe29fadfdeb2ae82f10b772e9aecfd
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-06 20:55:13 -0600

    changed format for request handling

--- test/thx/http/TestRequest.hx
+++ test/thx/http/TestRequest.hx
@@ -11,25 +11,36 @@ class TestRequest {
 		var done = Assert.createAsync(),
 				info = new RequestInfo(Get, "http://localhost:8081/", [
 						"Agent" => "thx.http.Request"
-					]),
-				handler = RequestHandler.create(function(status : Int, _) {
-					Assert.equals(200, status);
-					done();
-				});
+					]);
 
-		Request.make(info, handler);
+		Request.make(info,
+			function(r) {
+				Assert.equals(200, r.statusCode);
+				done();
+			},
+			function(e) Assert.fail("should never reach this point")
+		);
 	}
 
 	public function testStringBody() {
 		var message = "request thx body",
 				done = Assert.createAsync(),
-				info = new RequestInfo(Post, "http://localhost:8081/", BodyString(message)),
-				handler = RequestHandler.create(function(status : Int, body : String) {
-					Assert.equals(message, body);
-					Assert.equals(200, status);
-					done();
-				});
+				info = new RequestInfo(Post, "http://localhost:8081/", BodyString(message));
 
-		Request.make(info, handler);
+		Request.make(info,
+			function(r) {
+				trace(r.headers);
+				Assert.equals(200, r.statusCode);
+				var s = switch r.body {
+					case BodyString(s, _): s;
+					case BodyBytes(b): b.toString();
+					case BodyStream(s): s.readAll().toString();
+					case NoBody: null;
+				};
+				Assert.equals(message, s);
+				done();
+			},
+			function(e) Assert.fail("should never reach this point")
+		);
 	}
 }
