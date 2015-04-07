09de166e781f67c423b56ab3d1905383d358256b
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-06 20:58:24 -0600

    update Html5Request

--- src/thx/http/Request.hx
+++ src/thx/http/Request.hx
@@ -1,15 +1,16 @@
 package thx.http;
 
 import thx.core.Functions;
+import thx.core.Error;
 
 class Request {
-	public static function make(requestInfo : RequestInfo, requestHandler : RequestHandler) : Void -> Void {
+	public static function make(requestInfo : RequestInfo, callback : Response -> Void, error : Error -> Void) : Void -> Void {
 #if hxnodejs
-		return thx.http.core.NodeJSRequest.make(requestInfo, requestHandler);
+		return thx.http.core.NodeJSRequest.make(requestInfo, callback, error);
 #elseif js
-		return thx.http.core.Html5Request.make(requestInfo, requestHandler);
+		return thx.http.core.Html5Request.make(requestInfo, callback, error);
 #else
-		return thx.http.core.HaxeRequest.make(requestInfo, requestHandler);
+		return thx.http.core.HaxeRequest.make(requestInfo, callback, error);
 #end
 	}
 }
--- src/thx/http/core/Html5Request.hx
+++ src/thx/http/core/Html5Request.hx
@@ -3,12 +3,17 @@ package thx.http.core;
 import thx.http.Header;
 import js.html.XMLHttpRequest;
 using thx.core.Arrays;
+using thx.core.Error;
 
 class Html5Request {
-	public static function make(requestInfo : RequestInfo, requestHandler : RequestHandler) : Void -> Void {
+	public static function make(requestInfo : RequestInfo, callback : Response -> Void, error : Error -> Void) : Void -> Void {
 		var req = new XMLHttpRequest();
 		req.onload = function() {
-			requestHandler.statusHandler(req.status, req.responseText);
+			var res = new Html5Response(req.status, req.statusText, req.responseText, req.getAllResponseHeaders);
+			callback(res);
+		};
+		req.onerror = function(e) {
+			error(thx.core.Error.fromDynamic(e));
 		};
 		req.open(
 			requestInfo.method,
@@ -22,10 +27,44 @@ class Html5Request {
 			case BodyString(s, e):
 				req.send(s);
 			case BodyBytes(b):
-				req.send(b);
+				req.send(b); // TODO needs conversion
 			case BodyStream(s):
-				req.send(s);
+				try {
+					var b;
+					while((b = s.read(8192)).length > 0) {
+						req.send(b.getData());
+					}
+				} catch(e : haxe.io.Eof) {
+					req.send(); // TODO is this needed?
+				}
+				req.send(s); // TODO needs conversion
 			}
 		return function() {};
+	}
+}
+
+
+
+class Html5Response implements thx.http.Response {
+	@:isVar public var body(get, null) : ResponseBody;
+	@:isVar public var statusCode(get, null) : Int;
+	@:isVar public var statusText(get, null) : String;
+	@:isVar public var headers(get, null) : Headers;
+	var getRawHeaders : Void -> String;
+	public function new(statusCode : Int, statusText : String, body : String, getRawHeaders : Void -> String) {
+		this.statusCode = statusCode;
+		this.statusText = statusText;
+		this.body = null == body || "" == body ? NoBody : BodyString(body);
+		this.getRawHeaders = getRawHeaders;
+	}
+
+	function get_body() return body;
+	function get_statusCode() return statusCode;
+	function get_statusText() return null;
+	function get_headers() {
+		if(null == headers) {
+			headers = getRawHeaders();
+		}
+		return headers;
 	}
 }
