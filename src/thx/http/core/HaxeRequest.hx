ae7a19d3eec8d64097c8048e6a24efc72d8e33c7
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-04-06 20:58:11 -0600

    update HaxeRequest

--- src/thx/http/core/HaxeRequest.hx
+++ src/thx/http/core/HaxeRequest.hx
@@ -4,26 +4,68 @@ using thx.core.Arrays;
 import thx.core.Functions;
 
 class HaxeRequest {
-	public static function make(requestInfo : RequestInfo, requestHandler : RequestHandler) : Void -> Void {
+	public static function make(requestInfo : RequestInfo, callback : Response -> Void, error : thx.core.Error -> Void) : Void -> Void {
 		var req = new haxe.Http(requestInfo.url);
 		(requestInfo.headers : Array<Header>)
 			.pluck(req.addHeader(_.key, _.value));
 
-		var buf = "";
-		req.onData = function(data) buf += data;
-		req.onStatus = function(status) requestHandler.statusHandler(status, buf);
+		switch requestInfo.body {
+			case BodyString(s, _): // TODO encoding
+				req.setPostData(s);
+			case BodyBytes(b):
+				req.setPostData(b.toString());
+			case BodyStream(s):
+				req.setPostData(s.readAll().toString());
+			case NoBody: // do nothing
+		}
 
-		//req.onError = requestHandler.errorHandler;
+		var status = 0;
+		req.onData = function(data : String) {
+			var res = new HaxeResponse(status, data, #if js new haxe.ds.StringMap() #else req.responseHeaders #end);
+			callback(res);
+		};
+		req.onStatus = function(s) {
+			status = s;
+		};
+		req.onError = function(msg) {
+			error(new thx.core.Error(msg));
+		};
+
 		switch requestInfo.method {
 			case Get: req.request(false);
 			case Post: req.request(true);
 			case other: throw 'haxe.Http doesn\'t support method "$other"';
 		}
+
 		#if (flash || js)
 		return req.cancel;
 		#else
 		// no cancel, sorry
 		return Functions.noop;
 		#end
+	}
+}
+
+class HaxeResponse implements thx.http.Response {
+	@:isVar public var body(get, null) : ResponseBody;
+	@:isVar public var statusCode(get, null) : Int;
+	public var statusText(get, null) : String;
+	@:isVar public var headers(get, null) : Headers;
+	var responseHeaders : haxe.ds.StringMap<String>;
+
+	public function new(statusCode : Int, body : String, responseHeaders : haxe.ds.StringMap<String>) {
+		this.statusCode = statusCode;
+		this.body = null == body || "" == body ? NoBody : BodyString(body);
+		this.responseHeaders = responseHeaders;
+	}
+
+	function get_body() return body;
+	function get_statusCode() return statusCode;
+	function get_statusText() return null;
+	function get_headers() {
+		if(null == headers) {
+			headers = responseHeaders;
+		}
+		return headers;
 	}
 }
