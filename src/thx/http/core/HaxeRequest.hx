dd407e367703a24190c4f3b47f16b01fbbcd1d9a
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-05-22 21:14:52 -0600

    reimplemented HaxeRequest

--- src/thx/http/core/HaxeRequest.hx
+++ src/thx/http/core/HaxeRequest.hx
@@ -1,71 +1,58 @@
 package thx.http.core;
 
 using thx.Arrays;
-import thx.Functions;
+using thx.Strings;
+using thx.promise.Promise;
+using thx.stream.Value;
+using thx.stream.Emitter;
+import haxe.io.Bytes;
 
 class HaxeRequest {
-	public static function make(requestInfo : RequestInfo, callback : Response -> Void, error : thx.Error -> Void) : Void -> Void {
-		var req = new haxe.Http(requestInfo.url);
-		(requestInfo.headers : Array<Header>)
-			.pluck(req.addHeader(_.key, _.value));
+	public static function make(requestInfo : RequestInfo) : Promise<Response> {
+		return Promise.create(function(resolve : Response -> Void, reject) {
+			var req = new haxe.Http(requestInfo.url);
+			(requestInfo.headers : Array<Header>)
+				.pluck(req.addHeader(_.key, _.value));
 
-		switch requestInfo.body {
-			case BodyString(s, _): // TODO encoding
-				req.setPostData(s);
-			case BodyBytes(b):
-				req.setPostData(b.toString());
-			case BodyStream(s):
-				req.setPostData(s.readAll().toString());
-			case NoBody: // do nothing
-		}
+			switch requestInfo.body {
+				case BodyString(s, _): // TODO encoding
+					req.setPostData(s);
+				case BodyBytes(b):
+					req.setPostData(b.toString());
+				case BodyStream(s):
+					req.setPostData(s.readAll().toString());
+				case NoBody: // do nothing
+			}
 
-		var status = 0;
-		req.onData = function(data : String) {
-			var res = new HaxeResponse(status, data, #if js new haxe.ds.StringMap() #else req.responseHeaders #end);
-			callback(res);
-		};
-		req.onStatus = function(s) {
-			status = s;
-		};
-		req.onError = function(msg) {
-			error(new thx.Error(msg));
-		};
+			var value = new Value(null);
+			req.onData = function(data : String) {
+				if(!data.isEmpty())
+					value.set(Bytes.ofString(data));
+			};
+			req.onStatus = function(s) {
+				resolve(new HaxeResponse(s, value, req.responseHeaders));
+			};
+			req.onError = function(msg) {
+				trace('ERROR: $msg');
+				reject(new thx.Error(msg));
+			};
 
-		switch requestInfo.method {
-			case Get: req.request(false);
-			case Post: req.request(true);
-			case other: throw 'haxe.Http doesn\'t support method "$other"';
-		}
-
-		#if (flash || js)
-		return req.cancel;
-		#else
-		// no cancel, sorry
-		return Functions.noop;
-		#end
+			switch requestInfo.method {
+				case Get:   req.request(false);
+				case Post:  req.request(true);
+				case other: throw 'haxe.Http doesn\'t support method "$other"';
+			}
+		});
 	}
 }
 
 class HaxeResponse extends thx.http.Response {
-	@:isVar public var body(get, null) : ResponseBody;
-	@:isVar public var statusCode(get, null) : Int;
-	public var statusText(get, null) : String;
-	@:isVar public var headers(get, null) : Headers;
-	var responseHeaders : haxe.ds.StringMap<String>;
-
-	public function new(statusCode : Int, body : String, responseHeaders : haxe.ds.StringMap<String>) {
-		this.statusCode = statusCode;
-		this.body = null == body || "" == body ? NoBody : BodyString(body);
-		this.responseHeaders = responseHeaders;
+	var _statusCode : Int;
+	public function new(statusCode : Int, value : Value<Bytes>, headers : Headers) {
+		this._statusCode = statusCode;
+		this.headers = headers;
+		emitter = value;
 	}
 
-	function get_body() return body;
-	function get_statusCode() return statusCode;
-	function get_statusText() return null;
-	function get_headers() {
-		if(null == headers) {
-			headers = responseHeaders;
-		}
-		return headers;
-	}
+	override function get_statusCode() return _statusCode;
 }
