7b4bca09e8c0c0cd5bbe8b9c9ca945e0b4a0d403
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-06-27 22:31:26 -0600

    store body content and prepare for async input

--- src/thx/http/RequestInfo.hx
+++ src/thx/http/RequestInfo.hx
@@ -2,6 +2,8 @@ package thx.http;
 
 import thx.Url;
 import thx.http.RequestBody;
+import haxe.io.Bytes;
+using thx.promise.Promise;
 using thx.Strings;
 
 class RequestInfo {
@@ -47,13 +49,15 @@ class RequestInfo {
 	public var headers : Headers;
 	public var version : String;
 	public var body : RequestBody;
+	public var bytes : Bytes;
 
-	public function new(method : Method, url : Url, ?headers : Headers, ?body : RequestBody) {
+	public function new(method : Method, url : Url, ?headers : Headers, ?body : RequestBody, ?version = "1.1") {
 		this.method = method;
 		this.url = url;
 		this.headers = null == headers ? Headers.empty() : headers;
-		this.version = "1.1";
+		this.version = version;
 		this.body = null == body ? NoBody : body;
+		this.bytes = null;
 	}
 
 	public function toString() {
@@ -67,15 +71,34 @@ class RequestInfo {
 			buf.push('Host: ${url.host}');
 		if(h != "")
 			buf.push(h);
-		switch body {
-			case NoBody:
-			case BodyString(s, _):
-				buf.push(Const.CRLF + s);
-			case BodyBytes(b):
-				buf.push(Const.CRLF + b.toString());
-				buf.push(Const.CRLF + s.readAll().toString());
-			case BodyInput(s):
+		if(null == bytes) {
+			switch body {
+				case NoBody:
+				case BodyString(s, _):
+					buf.push(Const.CRLF + s);
+				case BodyBytes(b):
+					buf.push(Const.CRLF + b.toString());
+				case BodyInput(s):
+					buf.push(Const.CRLF + s.readAll().toString());
+			}
+		} else if(bytes.length > 0) {
+			buf.push(Const.CRLF + bytes.toString());
 		}
 		return buf.join(Const.CRLF);
+	}
+
+	public function read() : Promise<Nil> {
+		if(null != bytes) return Promise.nil;
+		switch body {
+			case NoBody:
+				bytes = Bytes.alloc(0);
+			case BodyString(s, _):
+				bytes = Bytes.ofString(s);
+			case BodyBytes(b):
+				bytes = b;
+			case BodyInput(s):
+				bytes = s.readAll();
+		}
+		return Promise.nil;
 	}
 }
