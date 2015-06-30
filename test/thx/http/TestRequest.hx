e563ff1b268c98d7f3c3af21c46014cc35f20447
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-06-29 21:07:52 -0600

    fixed test (and whitespaces)

--- test/thx/http/TestRequest.hx
+++ test/thx/http/TestRequest.hx
@@ -6,66 +6,67 @@ using thx.http.Request;
 using thx.stream.Emitter;
 
 class TestRequest {
-	public function new() {}
+  public function new() {}
 
-	public function testSimpleRequest() {
-		var done = Assert.createAsync(),
-				info = new RequestInfo(Get, "http://localhost:8081/", [
-						"Agent" => "thx.http.Request"
-					]);
+  public function testSimpleRequest() {
+    var done = Assert.createAsync(),
+        info = new RequestInfo(Get, "http://localhost:8081/", [
+            "Agent" => "thx.http.Request"
+          ]);
 
-		Request.make(info)
-			.mapSuccessPromise(function(r) {
-				Assert.equals(200, r.statusCode);
-				return r.asString();
-			})
-			.success(function(r) Assert.equals("OK", r))
-			.failure(function(e) Assert.fail("should never reach this point"))
-			.always(done);
-	}
+    Request.make(info)
+      .mapSuccessPromise(function(r) {
+        Assert.equals(200, r.statusCode);
+        return r.asString();
+      })
+      .success(function(r) Assert.equals("OK", r))
+      .failure(function(e) Assert.fail("should never reach this point"))
+      .always(done);
+  }
 
-	public function testQueryStringBody() {
-		var message = "request thx body",
-				done = Assert.createAsync(),
-				info = new RequestInfo(Post, 'http://localhost:8081/qs?q=$message', NoBody);
+  public function testQueryStringBody() {
+    var message = "request thx body",
+        done = Assert.createAsync(),
+        info = new RequestInfo(Post, 'http://localhost:8081/qs?q=$message', NoBody);
 
-		Request.make(info)
-			.mapSuccessPromise(function(r) {
-				Assert.equals(200, r.statusCode);
-				return r.asString();
-			})
-			.success(function(r) Assert.equals(message, r))
-			.failure(function(e) Assert.fail('$e'))
-			.always(done);
-	}
+    Request.make(info)
+      .mapSuccessPromise(function(r) {
+        Assert.equals(200, r.statusCode);
+        return r.asString();
+      })
+      .success(function(r) Assert.equals(message, r))
+      .failure(function(e) Assert.fail('$e'))
+      .always(done);
+  }
 
-	public function testStringBody() {
-		var message = "request thx body",
-				done = Assert.createAsync(),
-				info = new RequestInfo(Post, 'http://localhost:8081/json', BodyString('{"q":"$message"}'));
+  public function testStringBody() {
+    var message = "request thx body",
+        done = Assert.createAsync(),
+        info = new RequestInfo(Post, 'http://localhost:8081/json', BodyString('{"q":"$message"}'));
+    info.headers.add("Content-Type", "application/json");
 
-		Request.make(info)
-			.mapSuccessPromise(function(r) {
-				Assert.equals(200, r.statusCode);
-				return r.asString();
-			})
-			.success(function(r) Assert.equals(message, r))
-			.failure(function(e) Assert.fail('$e'))
-			.always(done);
-	}
+    Request.make(info)
+      .mapSuccessPromise(function(r) {
+        Assert.equals(200, r.statusCode);
+        return r.asString();
+      })
+      .success(function(r) Assert.equals(message, r))
+      .failure(function(e) Assert.fail('$e'))
+      .always(done);
+  }
 
-	public function testNoContent() {
-		var done = Assert.createAsync(),
-				info = new RequestInfo(Get, "http://localhost:8081/nocontent", [
-						"Agent" => "thx.http.Request"
-					]);
+  public function testNoContent() {
+    var done = Assert.createAsync(),
+        info = new RequestInfo(Get, "http://localhost:8081/nocontent", [
+            "Agent" => "thx.http.Request"
+          ]);
 
-		Request.make(info)
-			.success(function(r) {
-				//Assert.same(r.body, ResponseBody.NoBody);
-				Assert.equals(204, r.statusCode);
-				done();
-			})
-			.failure(function(e) Assert.fail("should never reach this point"));
-	}
+    Request.make(info)
+      .success(function(r) {
+        //Assert.same(r.body, ResponseBody.NoBody);
+        Assert.equals(204, r.statusCode);
+        done();
+      })
+      .failure(function(e) Assert.fail("should never reach this point"));
+  }
 }
