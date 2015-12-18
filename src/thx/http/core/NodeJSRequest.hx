cbe5e45d76cc87d4199c05543cf74ac3b2da509c
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-12-18 10:03:04 -0700

    made methods public (temp solution)

--- src/thx/http/core/NodeJSRequest.hx
+++ src/thx/http/core/NodeJSRequest.hx
@@ -84,7 +84,7 @@ class NodeJSRequest {
     return (untyped __js__("String")).fromCharCode.apply(null, new js.html.Uint16Array(ab));
   }
 
-  static function arrayBufferToBuffer(ab : js.html.ArrayBuffer) : Buffer {
+  public static function arrayBufferToBuffer(ab : js.html.ArrayBuffer) : Buffer {
     var buffer = new Buffer(ab.byteLength);
     var view = new js.html.Uint8Array(ab);
     for(i in 0...buffer.length)
@@ -109,7 +109,7 @@ class NodeJSResponse extends thx.http.Response {
     this.emitter = bus;
   }
 
-  static function bufferToArrayBuffer(buf : Buffer) : js.html.ArrayBuffer {
+  public static function bufferToArrayBuffer(buf : Buffer) : js.html.ArrayBuffer {
     var ab = new js.html.ArrayBuffer(buf.length);
     var view = new js.html.Uint8Array(ab);
     for(i in 0...buf.length)
