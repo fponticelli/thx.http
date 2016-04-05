3a0249c4484652e90e57e7af38f6f28910cf3fb8
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-04-04 22:26:06 -0600

    clear

--- src/thx/http/core/NodeJS.hx
+++ src/thx/http/core/NodeJS.hx
@@ -5,7 +5,6 @@ import js.node.Buffer;
 import haxe.io.Bytes;
 
 class NodeJS {
-
   public static function bufferToArrayBuffer(buf : Buffer) : ArrayBuffer {
     var ab = new ArrayBuffer(buf.length);
     var view = new js.html.Uint8Array(ab);
@@ -13,11 +12,6 @@ class NodeJS {
       view[i] = buf[i];
     return ab;
   }
-
-  // static function arrayBufferToString(ab : ArrayBuffer) : String {
-  //   return (untyped __js__("String")).fromCharCode.apply(null, new js.html.Uint16Array(ab));
-  // }
-  //
 
   public static function bufferToBytes(buf : Buffer) : Bytes {
     var ab = bufferToArrayBuffer(buf);
